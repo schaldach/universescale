48edbdac5caa3e058f956006f1e8d6407970f4f9
schaldach <gabimorgado0311@gmail.com>
2022-10-22 20:23:04 -0300

    trabalhando no layout e primeiras paginas

---
+++ components/LinkButton.js
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+
+function LinkButton({href, name, right, extraStyles}) {
+    return (
+        <Link href={href}>
+            <div className={'linkbutton '+extraStyles} style={{flexDirection:right?'row':'row-reverse'}}>
+                {name}
+                {right?
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+                </svg>:
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
+                </svg>
+                }
+            </div>
+        </Link>
+    );
+}
+
+export default LinkButton;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
+import LinkButton from '../components/LinkButton'
 
 export default function Home() {
   return (
@@ -9,22 +10,8 @@ export default function Home() {
         <div className='subtitle'>POR GABRIEL MORGADO</div>
       </div>
       <div className='landingbuttons'>
-        <Link href='/first'>
-          <div className='linkbutton'>
-            COMEÇAR
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
-            </svg>  
-          </div>
-        </Link>
-        <Link href='/credits'>
-          <div className='linkbutton'>
-            CRÉDITOS
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
-            </svg>  
-          </div>
-        </Link>
+        <LinkButton href='/first' name='COMEÇAR' right={true}/>
+        <LinkButton href='/credits' name='CRÉDITOS' right={true}/>
       </div>
     </div>
   )
