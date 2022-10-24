19b94be0781debaf6ea575c9af6495a44317496b
schaldach <gabimorgado0311@gmail.com>
2022-10-23 21:36:35 -0300

    animação no css oscilante belezura

--- pages/[slug].js
+++ pages/[slug].js
@@ -5,23 +5,25 @@ import {useState, useEffect} from 'react'
 
 function Astro({postData}) {
     const [shrinkAnimation, startAnimation] = useState(false)
+    const smallSize = postData.smallradius/postData.radius
+    const shrinkSize = postData.radius/postData.bigradius
     useEffect(() => startAnimation(false), [postData])
 
     return (
         <div className='page'>
-        <div className={shrinkAnimation?'astro darkpage':'astro fadepage'}>
-            <div className='imgbackground big'>
+        <div className={shrinkAnimation?'astro darkpage':'astro'}>
+            <div className='imgbackground big' style={{right:shrinkAnimation?'350px':'0px', transform:shrinkAnimation?`scale(${shrinkSize})`:'', transition: shrinkAnimation?'all 1s linear':''}}>
                 <Image src={postData.bigImage} layout='fixed' width='500px' height='500px'/>
             </div>
             <div className='imgbackground small'>
                 <div>
-                    <Image src={postData.smallImage} layout='fixed' width={`${Math.ceil(500*postData.smallradius/postData.bigradius)}px`} height={`${Math.ceil(500*postData.smallradius/postData.bigradius)}px`}/>
+                    <Image src={postData.smallImage} layout='fixed' width={`${Math.ceil(500*smallSize)}px`} height={`${Math.ceil(500*smallSize)}px`}/>
                     <div>{postData.smallertitle.toUpperCase()} PARA ESCALA</div>
                 </div>
             </div>
             <div className='astrotitle'>{postData.title.toUpperCase()}</div>
             <div className='astrodescription'>{postData.description.toUpperCase()}</div>
-            <LinkButton startAnimation={startAnimation} href={`/${postData.previousSlug}`} name='VOLTAR' right={false} extraStyles='left astrobutton'/>
+            <LinkButton href={`/${postData.previousSlug}`} name='VOLTAR' right={false} extraStyles='left astrobutton'/>
             <LinkButton startAnimation={startAnimation} href={`/${postData.nextSlug}`} name='AMPLIAR' right={true} extraStyles='right astrobutton'/>
         </div>
         </div>
